e93833d1937fc2fe48c1f6309676c78bef438606
KazakovMaksim <kazakovmaksimi@gmail.com>
2025-04-04 09:15:01 +0400

    feat(*): add inter font

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import './index.css';
 
+import { extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -7,10 +9,26 @@ import { Provider } from 'react-redux';
 import App from '~/app/App.tsx';
 import { store } from '~/store/configure-store.ts';
 
+const theme = extendTheme({
+    colors: {
+        lime: {
+            50: '#ffffd3',
+            150: '#d7ff94',
+            300: '#c4ff61',
+            600: '#2db100',
+        },
+    },
+    fonts: {
+        main: 'Inter, sans-serif',
+    },
+});
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <App />
+            <ChakraProvider theme={theme}>
+                <App />
+            </ChakraProvider>
         </Provider>
     </StrictMode>,
 );
